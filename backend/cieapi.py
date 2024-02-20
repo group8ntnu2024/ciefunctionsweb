9fd3bfb690995838b5d3d67eb6f34e6be2d9a753
lars <lehoff@ntnu.no>
2024-02-20 04:47:30 +0100

    Made a functional api

--- backend/cieapi.py
+++ backend/cieapi.py
@@ -1,7 +1,29 @@
 from flask import Flask, request, jsonify
-from compute import compute_tabulated  # Import the compute_tabulated function from compute.py
+from compute import compute_tabulated
+import numpy as np
 
 api = Flask(__name__)
+
+# Convert results and plots to JSON serializable format
+def convert_to_json_serializable(data):
+    # Convert numpy arrays to lists
+    if isinstance(data, dict):
+        for key, value in data.items():
+            if isinstance(value, np.ndarray):
+                data[key] = value.tolist()
+            elif isinstance(value, dict):
+                convert_to_json_serializable(value)
+    # Lists that might contain numpy arrays
+    elif isinstance(data, list):
+        for i, item in enumerate(data):
+            if isinstance(item, np.ndarray):
+                data[i] = item.tolist()
+            elif isinstance(item, dict):
+                convert_to_json_serializable(item)
+
+
+
+
 
 class VisualDataAPI:
     def __init__(self):
@@ -10,34 +32,32 @@ class VisualDataAPI:
 # Checks the type of claculation to be performed
     def compute_visual_data(self, data):
         if 'type' in data and data['type'] == 'specific_computation':
-            result = self.compute_tabulated(data)
+            results, plots = self.compute(data)
+        elif 'type' in data and data['type'] == 'default_computation':
+            results, plots = self.compute_default()
         else:
-            result = self.compute_default_tabulated(data)
-        return result
+            results, plots = {"error": "Invalid computation type specified."}, {}            
+        return results, plots
 
 # Computes the default tabulated data
-    def compute_default_tabulated(self, data=None):
-        field_size = 0
-        age = 0
-        λ_min = 390
-        λ_max = 830
-        λ_step = 1
-        
-        #removed for testing purposes
-        #return compute_tabulated(field_size, age, λ_min, λ_max, λ_step)
-        return age + λ_min + λ_max
+    def compute_default(self):
+        field_size, age, λ_min, λ_max, λ_step = 0, 0, 390, 830, 1
+        results, plots = compute_tabulated(field_size, age, λ_min, λ_max, λ_step)
+        convert_to_json_serializable(results)
+        convert_to_json_serializable(plots)
+        return results, plots
 
 # Computes the tabulated data
-    def compute_tabulated(self, data):
-        field_size = int(data.get('field_size', 0))
-        age = int(data.get('age', 0))
-        λ_min = int(data.get('min', 390))
-        λ_max = int(data.get('max', 830))
-        λ_step = int(data.get('step', 1))
-
-        #removed for testing purposes
-        #return compute_tabulated(field_size, age, λ_min, λ_max, λ_step)
-        return age + λ_min + λ_max
+    def compute(self, data):
+        field_size = float(data.get('field_size', 0))
+        age = float(data.get('age', 0))
+        λ_min = float(data.get('min', 390))
+        λ_max = float(data.get('max', 830))
+        λ_step = float(data.get('step', 1))
+        results, plots = compute_tabulated(field_size, age, λ_min, λ_max, λ_step)
+        convert_to_json_serializable(results)
+        convert_to_json_serializable(plots)
+        return results, plots
 
 
 
@@ -46,15 +66,33 @@ class VisualDataAPI:
 def compute_visual_data_endpoint():
     data_api = VisualDataAPI()
     data = request.json
-    result = data_api.compute_visual_data(data)
-    return jsonify(result)
+    results, plots = data_api.compute_visual_data(data)
+    return jsonify({'results': results, 'plots': plots})
 
 # Endpoint to compute the default data
 @api.route('/compute_default_visual_data', methods=['GET'])
 def compute_default_visual_data_endpoint():
     data_api = VisualDataAPI()
-    result = data_api.compute_default_tabulated()
-    return jsonify(result)
+    results, plots = data_api.compute_default()
+    return jsonify({'results': results, 'plots': plots})
 
 if __name__ == '__main__':
     api.run(debug=True)
+
+
+
+#========================================
+#===============JSON BODY================
+#   {
+#    "field_size": 1,
+#    "age": 99,
+#    "min": 390,
+#    "max": 830,
+#    "step": 1,
+#    "type": "specific_computation"
+#   }
+#
+#
+#==================Header=================
+#   key: content-type
+#   value: application/json
