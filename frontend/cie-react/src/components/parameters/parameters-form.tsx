20d78e1d166f1448bb1a767bf4a50cb850c2e4e4
andersbm <anderbm@stud.ntnu.no>
2024-02-28 05:01:00 +0100

    updated post request url in parameters-form.tsx for compatibility

--- frontend/cie-react/src/components/parameters/parameters-form.tsx
+++ frontend/cie-react/src/components/parameters/parameters-form.tsx
@@ -16,7 +16,7 @@ const ParametersForm = () => {
     };
 
     try {
-      const response = await fetch('http://localhost:5000/compute_all_specific_data', {
+      const response = await fetch('http://127.0.0.1:5000/compute_all_specific_data', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
