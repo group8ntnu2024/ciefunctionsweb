5a63b8e066f7a1215f6bb9d265f3b8d75eb45989
andersbm <anderbm@stud.ntnu.no>
2024-04-14 22:48:35 +0200

    Merge fix. Delete imports

--- frontend/cie-react/src/components/SidePanel/SidePanel.tsx
+++ frontend/cie-react/src/components/SidePanel/SidePanel.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
 import './SidePanel.css';
-import 'katex/dist/katex.min.css';
-import { InlineMath } from 'react-katex';
-import 'katex/dist/katex.min.css';
 
 interface SidePanelProps {
   selectedOption: string;
@@ -20,7 +17,6 @@ export const SidePanel: React.FC<SidePanelProps> = ({ selectedOption }) => {
                 Age: 32 yr</p>
                 <p><strong>Function symbols</strong></p>
                 <p>Functions:</p>
-                <InlineMath math={'\\bar{l}_{2,32} \\quad \\bar{m}_{2,32} \\quad \\bar{s}_{2,32}'} />
                 <p>Argument: λ (wavelength)</p>
                 <p><strong>Wavelengths</strong></p>
                 <p>Domain: 390 nm – 830 nm<br/>
@@ -40,7 +36,6 @@ export const SidePanel: React.FC<SidePanelProps> = ({ selectedOption }) => {
                 Age: 32 yr</p>
                 <p><strong>Function symbols</strong></p>
                 <p>Functions:</p>
-                <InlineMath math={'\\bar{l}_{2,32} \\quad \\bar{m}_{2,32} \\quad \\bar{s}_{2,32}'} />
                 <p>Argument: λ (wavelength)</p>
                 <p><strong>Wavelengths</strong></p>
                 <p>Domain: 390 nm – 830 nm<br/>
@@ -59,7 +54,6 @@ export const SidePanel: React.FC<SidePanelProps> = ({ selectedOption }) => {
                   <p>Field size: 2°<br/>
                   Age: 32 yr</p>
                   <p><strong>Coordinate symbols</strong></p>
-                  <InlineMath math={'\\ell_{MB,2,32}, m_{MB,2,32}, s_{MB,2,32}'} />
                   <p><strong>Wavelengths</strong></p>
                   <p>Domain: 390 nm – 830 nm<br/>
                   Step: 1 nm</p>
