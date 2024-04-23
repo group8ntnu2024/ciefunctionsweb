41e8c3ac218601cb9ac97e621413012f2e1c1d5b
Lars Hoff <lars@Larss-MacBook-Pro.local>
2024-04-23 22:51:52 +0200

    added wavelenght points to CIE xy standard chromaticity diagram)

--- frontend/cie-react/src/components/plot/XyStandardChromaticityDiagram.tsx
+++ frontend/cie-react/src/components/plot/XyStandardChromaticityDiagram.tsx
@@ -7,110 +7,155 @@ import { MethodOption, titles } from '../../utils/propTypes';
 import { Data } from 'plotly.js';
 
 const XyStandardChromaticityDiagram: React.FC = () => {
-  const { computedData, isLoading } = useParameters();
-  const { selectedOption } = useContentController();
-
-  const plotTitle = (selectedOption && titles[selectedOption as MethodOption]) ? titles[selectedOption as MethodOption] : 'Chromaticity Diagram';
-
-  if (!computedData || !computedData.plotData || computedData.plotData.length === 0) {
-    return <div>No data available for plotting.</div>;
-  }
-
-  if (isLoading) {
-    return <LoadingIndicator />;
-  }
-
-  const xValues = computedData.plotData.map(item => item[1]); 
-  const yValues = computedData.plotData.map(item => item[2]);
-
-  // Prepares plot data with in the arch of the chromaticity diagram
-  const chartData: Data[] = [
-    {
-      x: xValues,
-      y: yValues,
-      type: 'scatter',
-      mode: 'lines',
-      line: {
-        color: 'black',
-        width: 2
-      }
+    const { computedData, isLoading } = useParameters();
+    const { selectedOption } = useContentController();
+  
+    const plotTitle = (selectedOption && titles[selectedOption as MethodOption]) ? titles[selectedOption as MethodOption] : 'Chromaticity Diagram';
+  
+    if (!computedData || !computedData.plotData || computedData.plotData.length === 0) {
+      return <div>No data available for plotting.</div>;
     }
-  ];
-
-  // Extracting purple line points if available and adding it to the plot
-  if (computedData.purpleLineData) {
-    const purpleXValues = computedData.purpleLineData.map(item => item[1]);
-    const purpleYValues = computedData.purpleLineData.map(item => item[2]);
-
-    chartData.push({
-      x: purpleXValues,
-      y: purpleYValues,
-      type: 'scatter',
-      mode: 'lines',
-      line: {
-        color: 'black',
-        width: 2
+  
+    if (isLoading) {
+      return <LoadingIndicator />;
+    }
+  
+    function addSpecificWavelengthPoints() {
+      const startWavelength = 470;
+      const endWavelength = 611;
+      const step = 10;
+      const additionalWavelengths = [700];
+      drawnPoints.push(0);
+      drawnPoints.push(computedData.plotData.length - 1);
+  
+      for (let wavelength = startWavelength; wavelength <= endWavelength; wavelength += step) {
+          const index = wavelengths.indexOf(wavelength);
+          if (index !== -1) {
+              drawnPoints.push(index);
+          }
       }
-    });
+  
+      additionalWavelengths.forEach(wavelength => {
+          const index = wavelengths.indexOf(wavelength);
+          if (index !== -1) {
+              drawnPoints.push(index);
+          }
+      });
   }
-
+  
+    const xValues = computedData.plotData.map(item => item[1]); 
+    const yValues = computedData.plotData.map(item => item[2]);
+    const wavelengths = computedData.plotData.map(item => item[0]);
+  
+    const drawnPoints: number[] = [];
+    addSpecificWavelengthPoints();
+  
+    // Prepares plot data with in the arch of the chromaticity diagram
+    const chartData: Data[] = [
+      {
+        x: xValues,
+        y: yValues,
+        type: 'scatter',
+        mode: 'lines',
+        line: {
+          color: 'black',
+          width: 2
+        }
+      }
+    ];
+  
+    // Extracting purple line points if available and adding it to the plot
+    if (computedData.purpleLineData) {
+      const purpleXValues = computedData.purpleLineData.map(item => item[1]);
+      const purpleYValues = computedData.purpleLineData.map(item => item[2]);
+  
+      chartData.push({
+        x: purpleXValues,
+        y: purpleYValues,
+        type: 'scatter',
+        mode: 'lines',
+        line: {
+          color: 'black',
+          width: 2
+        }
+      });
+    }
+  
     // Adding the white point if available
     if (computedData.whitePointData) {
-        const whiteX = [computedData.whitePointData[0]];
-        const whiteY = [computedData.whitePointData[2]];
-    
-        chartData.push({
-          x: whiteX,
-          y: whiteY,
-          type: 'scatter',
-          mode: 'markers',
-          marker: {
-            color: 'red',
-            symbol: 'x-thin',
-            size: 10,
-            line: {
-              color: 'black',
-              width: 1
-            }
+      const whiteX = [computedData.whitePointData[0]];
+      const whiteY = [computedData.whitePointData[2]];
+  
+      chartData.push({
+        x: whiteX,
+        y: whiteY,
+        type: 'scatter',
+        mode: 'markers',
+        marker: {
+          color: 'red',
+          symbol: 'x-thin',
+          size: 10,
+          line: {
+            color: 'black',
+            width: 1
           }
-        });
-      }
-
-  const minX = Math.min(...xValues) - 0.1;
-  const maxX = Math.max(...xValues) + 0.1;
-  const minY = Math.min(...yValues) - 0.1;
-  const maxY = Math.max(...yValues) + 0.1;
-
-  try {
-    return (
-      <Plot
-        data={chartData}
-        layout={{
-          width: 800,
-          height: 600,
-          title: plotTitle,
-          xaxis: {
-            title: 'Chromaticity x',
-            range: [minX, maxX]
+        }
+      });
+    }
+  
+  
+    drawnPoints.forEach(index => {
+      chartData.push({
+        x: [xValues[index]],
+        y: [yValues[index]],
+        type: 'scatter',
+        mode: 'markers',
+        marker: {
+          color: 'black',
+          size: 10,
+          line: {
+            color: 'black',
+            width: 2
           },
-          yaxis: {
-            title: 'Chromaticity y',
-            range: [minY, maxY]
-          },
-          autosize: false,
-          margin: { l: 50, r: 10, b: 50, t: 50, pad: 4 },
-          showlegend: false
-        }}
-        config={{
-          scrollZoom: true,
-          displaylogo: false
-        }}
-      />
-    );
-  } catch (error) {
-    console.error("Error rendering plot:", error);
-    return <div>Error displaying plot. Please check the console for more details.</div>;
-  }
-};
-
+          symbol: 'circle'
+        }
+      });
+    });
+    
+    const minX = Math.min(...xValues) - 0.1;
+    const maxX = Math.max(...xValues) + 0.1;
+    const minY = Math.min(...yValues) - 0.1;
+    const maxY = Math.max(...yValues) + 0.1;
+  
+    try {
+      return (
+        <Plot
+          data={chartData}
+          layout={{
+            width: 800,
+            height: 600,
+            title: plotTitle,
+            xaxis: {
+              title: 'Chromaticity x',
+              range: [minX, maxX]
+            },
+            yaxis: {
+              title: 'Chromaticity y',
+              range: [minY, maxY]
+            },
+            autosize: false,
+            margin: { l: 50, r: 10, b: 50, t: 50, pad: 4 },
+            showlegend: false
+          }}
+          config={{
+            scrollZoom: true,
+            displaylogo: false
+          }}
+        />
+      );
+    } catch (error) {
+      console.error("Error rendering plot:", error);
+      return <div>Error displaying plot. Please check the console for more details.</div>;
+    }
+  };
 export default XyStandardChromaticityDiagram;
