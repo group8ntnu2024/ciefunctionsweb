dcdaae9c5ad529672ff1dfe4ba03966bf5ee5e0c
Anders Brunsberg Mariendal <anderbm@ntnu.no>
2024-05-17 21:16:27 +0200

    Update urlf for current VM config

--- frontend/cie-react/src/utils/ApiUrls.tsx
+++ frontend/cie-react/src/utils/ApiUrls.tsx
@@ -1,3 +1,3 @@
-export const SANIC_BASE_URL = "http://192.168.103.108:8000/api/v2/"
+export const SANIC_BASE_URL = "http://10.212.136.66:8000/api/v2/"
 export const LMS_CALC_URL = "lms/calculation"
 
