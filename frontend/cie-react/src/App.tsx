ef5ddc43ce1dfa1891ffd28f324027aa2ca68fc0
andersbm <anderbm@stud.ntnu.no>
2024-05-21 04:43:29 +0200

    Added licensing to every source file

--- frontend/cie-react/src/App.tsx
+++ frontend/cie-react/src/App.tsx
@@ -1,3 +1,22 @@
+/*App.tsx: Application component
+
+Copyright (C) 2012-2020 Ivar Farup and Jan Henrik Wold
+Copyright (C) 2024 Bachelor Thesis Group 8
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see http://www.gnu.org/licenses/.
+*/
+
 import { createHashRouter, Navigate, Outlet, RouterProvider } from "react-router-dom";
 import Navbar from "./components/navbar/NavbarComponent.tsx";
 import { PLOT_ROUTE, TABLE_ROUTE } from "./utils/router-urls.tsx";
--- frontend/cie-react/src/components/navbar/NavbarComponent.tsx
+++ frontend/cie-react/src/components/navbar/NavbarComponent.tsx
@@ -1,4 +1,21 @@
+/*NavbarComponent.tsx: Renders Navbar component
 
+Copyright (C) 2012-2020 Ivar Farup and Jan Henrik Wold
+Copyright (C) 2024 Bachelor Thesis Group 8
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see http://www.gnu.org/licenses/.
+*/
 import { Link } from 'react-router-dom';
 import './navbar.css'
 import { PLOT_ROUTE, TABLE_ROUTE } from '../../utils/router-urls';
